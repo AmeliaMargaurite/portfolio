cb236231530f35edc9caf578d3e0ab06fac345a7
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2022-11-03 13:25:51 +0100

    fixing paths to static/public folder

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -19,11 +19,11 @@ export default function Picture({
 			<img
 				className={className}
 				srcSet="
-          /images/Group-1_1_ci7b7s_c_scale,w_200.png 200w,
-          /images/Group-1_1_ci7b7s_c_scale,w_770.png 770w,
-          /images/Group-1_1_ci7b7s_c_scale,w_1137.png 1137w,
-          /images/Group-1_1_ci7b7s_c_scale,w_1400.png 1400w"
-				src="/images/Group-1_1_ci7b7s_c_scale,w_1400.png"
+          ../images/Group-1_1_ci7b7s_c_scale,w_200.png 200w,
+          ../images/Group-1_1_ci7b7s_c_scale,w_770.png 770w,
+          ../images/Group-1_1_ci7b7s_c_scale,w_1137.png 1137w,
+          ../images/Group-1_1_ci7b7s_c_scale,w_1400.png 1400w"
+				src="../images/Group-1_1_ci7b7s_c_scale,w_1400.png"
 				alt={alt}
 			/>
 		</picture>
